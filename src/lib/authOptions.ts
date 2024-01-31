8723824ffea62f61870e8524c4e400f455073b82
uncleaar <danilyudin723@gmail.com>
2024-01-31 22:23:27 +0700

    github auth

--- src/lib/authOptions.ts
+++ src/lib/authOptions.ts
@@ -2,13 +2,22 @@ import GoogleProvider from "next-auth/providers/google";
 import { FirestoreAdapter } from "@auth/firebase-adapter";
 import { firestore } from "./firestore";
 import { Adapter } from "next-auth/adapters";
+import { NextAuthOptions } from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
 
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
+  session: {
+    strategy: "jwt",
+  },
   adapter: FirestoreAdapter(firestore) as Adapter,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
     }),
   ],
 };
--- src/shared/ui/buttons/LoginButton/LoginButton.tsx
+++ src/shared/ui/buttons/LoginButton/LoginButton.tsx
@@ -1,16 +1,30 @@
 "use client";
 import { signIn } from "next-auth/react";
+import { useSearchParams } from "next/navigation";
 import React from "react";
 import { IoIosLogIn } from "react-icons/io";
 
 export const LoginButton = () => {
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get("callbackUrl") || "/";
   return (
-    <button
-      type="button"
-      className="text-black rounded-sm bg-gray-300 flex items-center gap-2 hover:bg-gray-400 hover:transition-all duration-300"
-      onClick={() => signIn("google")}
-    >
-      login <IoIosLogIn />
-    </button>
+    <div className="flex items-center gap-2">
+      <button
+        type="button"
+        className="text-black rounded-sm bg-gray-300 flex items-center gap-2 hover:bg-gray-400 hover:transition-all duration-300"
+        onClick={() => signIn("google", { callbackUrl })}
+      >
+        login google
+        <IoIosLogIn />
+      </button>
+
+      <button
+        type="button"
+        className="text-black rounded-sm bg-gray-300 flex items-center gap-2 hover:bg-gray-400 hover:transition-all duration-300"
+        onClick={() => signIn("github", { callbackUrl })}
+      >
+        login github <IoIosLogIn />
+      </button>
+    </div>
   );
 };
