e56fe7c0033391bf1e0eb9a1acc2893ec690b264
uncleaar <danilyudin723@gmail.com>
2024-02-01 01:26:02 +0700

    create board

--- src/app/actions/board-actions.ts
+++ src/app/actions/board-actions.ts
@@ -1,11 +1,30 @@
 "use server";
+import { authOptions } from "@/lib/authOptions";
+import { Liveblocks, RoomInfo } from "@liveblocks/node";
+import { getServerSession } from "next-auth";
+import uniqid from "uniqid";
 
-type createBoardForm = {
-  id: string;
-};
+export async function createBoard(name: string): Promise<boolean | RoomInfo> {
+  const liveblocks = new Liveblocks({
+    secret: process.env.LIVEBLOCKS_SECRET_KEY as string,
+  });
 
-export async function createBoard(name: string): Promise<createBoardForm> {
-  return {
-    id: "1",
-  };
+  const session = await getServerSession(authOptions);
+
+  const email = session?.user?.email || "";
+
+  if (email) {
+    const roomId = uniqid.time();
+    return await liveblocks.createRoom(roomId, {
+      defaultAccesses: [],
+      usersAccesses: {
+        [email]: ["room:write"],
+      },
+      metadata: {
+        boardName: name,
+      },
+    });
+  }
+
+  return false;
 }
